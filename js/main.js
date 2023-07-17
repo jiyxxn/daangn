8c81d6e728363e933ba104e8b4628d1b663c896c
jiyxxn <132261550+jiyxxn@users.noreply.github.com>
2023-07-17 19:29:43 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -182,19 +182,20 @@ bannerFrame.addEventListener('transitionend', () => {
 window.onload=function(){
   const text_more_wrap = document.querySelector('.text_more_wrap');
   const text_downsize_wrap = document.querySelector('.text_downsize_wrap');
-  const wrap_02_sht = document.querySelector('.wrap_02_sht');
+  const wrap_02_sht = document.querySelector('.wrap_sht_02');
 
   console.log(text_more_wrap, wrap_02_sht, text_downsize_wrap);
 
   text_more_wrap.addEventListener('click', function(){
     wrap_02_sht.style.display = 'block';
-    text_more_wrap.style.opacity = '0';
+    text_more_wrap.style.display = "none";
+    text_downsize_wrap.style.display = 'block';
   })
   
   text_downsize_wrap.addEventListener('click', function(){
     wrap_02_sht.style.display = 'none';
-    text_more_wrap.style.opacity = '100';
-    // text_more_wrap의 css를 display : block으로 했더니 기존의 flex가 망가지는 논리구조가 발생함
+    text_more_wrap.style.display = "block";
+    text_downsize_wrap.style.display = 'none';
   })
 }
 
