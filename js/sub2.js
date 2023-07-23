7fba917e65f091de2d05fa6cf7d2b45c7064b8d2
jisu <132331661+imsjisu@users.noreply.github.com>
2023-07-23 10:10:53 +0900

    Add files via upload

---
+++ js/sub2.js
@@ -0,0 +1,63 @@
+// 점핑이벤트 삭제
+document.addEventListener("click", (e) => {
+  e.preventDefault();
+});
+
+
+/* 판매내역 클릭 이벤트 */
+
+// 판매중
+const salesTitleBtn = document.querySelector("dt.sales_title");
+const salesCont = document.querySelector("dd.sales_cont");
+
+salesTitleBtn.addEventListener("click", (e) => {
+  salesTitleBtn.classList.add("on");
+  salesCont.classList.add("on");
+  // 거래완료,숨김 on제거
+  dealTitleBtn.classList.remove("on");
+  dealCont.classList.remove("on");
+  hideTitleBtn.classList.remove("on");
+  hideCont.classList.remove("on");
+})
+
+// 거래완료
+const dealTitleBtn = document.querySelector("dt.deal_title");
+const dealCont = document.querySelector("dd.deal_cont");
+
+dealTitleBtn.addEventListener("click", (e) => {
+  dealTitleBtn.classList.add("on");
+  dealCont.classList.add("on");
+  // 판매중,숨김 on제거
+  salesTitleBtn.classList.remove("on");
+  salesCont.classList.remove("on");
+  hideTitleBtn.classList.remove("on");
+  hideCont.classList.remove("on");
+})
+
+// 숨김
+const hideTitleBtn = document.querySelector("dt.hide_title");
+const hideCont = document.querySelector("dd.hide_cont");
+
+hideTitleBtn.addEventListener("click", (e) => {
+  hideTitleBtn.classList.add("on");
+  hideCont.classList.add("on");
+  // 판매중,거래완료 on제거
+  salesTitleBtn.classList.remove("on");
+  salesCont.classList.remove("on");
+  dealTitleBtn.classList.remove("on");
+  dealCont.classList.remove("on");
+})
+
+
+/* 판매중 content - 클릭드래그 슬라이드 */
+$(document).ready(function() {
+
+  $('.slides').slick({
+    arrows: false,
+    dots: false,
+    infinite: false,
+    speed: 400,
+    slidesToShow: 3.3,
+    slidesToScroll: 3,
+  })
+});
